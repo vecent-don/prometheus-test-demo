6c4ca48c400870161be4bdb9e77e8e4ac31ce0f5
zhyee <1181862765@qq.com>
2020-07-23 15:03:12 +0800

    update springboot version

--- src/test/java/io/daocloud/prometheustestdemo/PrometheusTestDemoApplicationTests.java
+++ src/test/java/io/daocloud/prometheustestdemo/PrometheusTestDemoApplicationTests.java
@@ -1,6 +1,6 @@
 package io.daocloud.prometheustestdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
